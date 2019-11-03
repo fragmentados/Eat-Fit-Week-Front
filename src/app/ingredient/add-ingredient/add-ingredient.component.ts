c49ccaaf56b3ee7970fdb86a989e732ab637a7ad
Elías <eliasfb94@hotmail.com>
2019-11-03 23:40:06 +0100

    [FEATURE] Allow to create dish with just one ingredient

--- src/app/ingredient/add-ingredient/add-ingredient.component.ts
+++ src/app/ingredient/add-ingredient/add-ingredient.component.ts
@@ -1,3 +1,5 @@
+import { AddDish } from './../../models/dish/addDish.model';
+import { DishRestService } from './../../dish/dishRest.service';
 import { FoodCategory } from './../../models/ingredient/foodCategory.model';
 import { UserService } from '../../user.service';
 import { AddIngredient } from '../../models/ingredient/addIngredient.model';
@@ -6,8 +8,10 @@ import { Router } from '@angular/router';
 import { IngredientService } from '.././ingredient.service';
 import { User } from '../../models/user/user.model';
 import { Stat } from '../../models/nutrition/stat.model';
-import { UNIQUE_CONSTRAINT_CODE, OK_CODE, DEFAULT_LANG } from '../../models/service';
+import { OK_CODE, DEFAULT_LANG } from '../../models/service';
 import { TranslateService } from '@ngx-translate/core';
+import { AddDishIngredient } from '../../models/dish/addDishIngredient.model';
+import { Meal } from '../../models/dish/meal.model';
 
 @Component({
   templateUrl: './add-ingredient.component.html',
@@ -20,11 +24,15 @@ export class AddIngredientComponent implements OnInit {
   bannedCategories: FoodCategory[];
   currentUser: User;
   categorySelectedBanned = false;
+  alsoAddDish = false;
+  ingredientQuantity: number;
+  userMeals: Meal[];
 
   constructor(private translate: TranslateService, private userService: UserService,
-    private router: Router, private ingredientService: IngredientService) {
+    private ingredientService: IngredientService, private dishService: DishRestService) {
     this.translate.setDefaultLang(DEFAULT_LANG);
     this.currentUser = this.userService.currentUserValue;
+    this.userService.getUserMeals(this.currentUser.id).subscribe(data => this.userMeals = data);
   }
 
   ngOnInit(): void {
@@ -53,6 +61,8 @@ export class AddIngredientComponent implements OnInit {
     this.ingredient.carbohydrates = null;
     this.ingredient.proteins = null;
     this.ingredient.fats = null;
+    this.ingredientQuantity = 0;
+    this.alsoAddDish = false;
   }
 
   createIngredient(): void {
@@ -60,12 +70,28 @@ export class AddIngredientComponent implements OnInit {
     this.ingredientService.createIngredient(this.ingredient)
         .subscribe( data => {
           if (data.errorCode === OK_CODE) {
-            this.clearForm();
-            this.translate.get('COMMON.INGREDIENT_CREATED').subscribe(trans => alert(trans));
+            if (this.alsoAddDish) {
+              this.dishService.createDish(this.createAddDishFromIngredient(data.entityId)).subscribe(() => {
+                this.clearForm();
+                this.translate.get('COMMON.INGREDIENT_CREATED').subscribe(trans => alert(trans));
+              });
+            } else {
+              this.clearForm();
+              this.translate.get('COMMON.INGREDIENT_CREATED').subscribe(trans => alert(trans));
+            }
           } else {
             alert(data.message);
           }
         });
+  }
+
+  createAddDishFromIngredient(ingredientId: number): AddDish {
+    const result: AddDish = new AddDish();
+    result.userId = this.currentUser.id;
+    result.name = this.ingredient.name;
+    result.ingredients = [new AddDishIngredient(ingredientId, this.ingredientQuantity)];
+    result.meals = this.userMeals.map(m => m.id);
+    return result;
   }
 
 }
--- src/app/models/backendResponse.model.ts
+++ src/app/models/backendResponse.model.ts
@@ -1,4 +1,5 @@
 export class BackendResponse {
   errorCode: number;
+  entityId: number;
   message: string;
 }
